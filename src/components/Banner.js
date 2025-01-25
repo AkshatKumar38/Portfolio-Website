d36d1bec916d2600c2059880a1c792da4a8a2524
AkshatKumar38 <kumarrr.akshat@gmail.com>
2025-01-26 02:12:20 +0530

    added minor adjustments

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -56,8 +56,8 @@ export const Banner = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
-                <h1>{`Hi! I'm Akshat`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Data Analyst", "Full Stack Developer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <h1>{`Hi! I'm Akshat`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Full Stack Developer", "Data Anayst", "Python Developer ]'><span className="wrap">{text}</span></span></h1>
+                  <p> Hi! I’m a passionate Full Stack Developer with a knack for building efficient, scalable, and user-friendly web applications. Alongside my expertise in front-end and back-end development, I’m skilled in Python and Data Analytics, enabling me to solve complex problems and make data-driven decisions. Whether it’s crafting seamless user experiences or diving into data to uncover insights, I thrive on creating impactful solutions.</p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,13 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
+import snake_game from "../assets/img/project/snake_game.png";
+import platform_shooter from "../assets/img/project/platform_shooter.png";
+import game_of_life from "../assets/img/project/game_of_life.png";
+import budget_buddy from "../assets/img/project/budget_buddy.png";
+import reversi from "../assets/img/project/reversi.png";
+import resume_screener from "../assets/img/project/resume_screener.png";
+import smart_safty_helmet from "../assets/img/project/smart_safty_helmet.png";
+import stock_manage from "../assets/img/project/stock_manage.png";
 import projImg2 from "../assets/img/project-img2.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
@@ -11,29 +18,44 @@ export const Projects = () => {
 
   const projects = [
     {
+      title: "A Platform Shooter Game",
+      description: "A fun to Play Platform shooter game along with a level editor for the game",
+      imgUrl: platform_shooter,
+    },
+    {
       title: "Budget Buddy",
       description: "A Group Expenses Management Website",
-      imgUrl: projImg1,
+      imgUrl: budget_buddy,
+    },
+    {
+      title: "Conway's Game of Life",
+      description: "A recreation of THe Conway's game of life for Personal Enjoyment",
+      imgUrl: game_of_life,
     },
     {
       title: "Smart Safety Helmet Wesbite",
       description: "A Website display the use of Our Smart Helmet for Miner Saftey",
-      imgUrl: projImg2,
+      imgUrl: smart_safty_helmet,
     },
     {
       title: "Reversi AI Game",
       description: "A fun Board Game to play when Bored",
-      imgUrl: projImg2,
+      imgUrl: reversi,
+    },
+    {
+      title: "Snake AI Game",
+      description: "A fun Game to play as well to showcase AI reinforced learning",
+      imgUrl: snake_game,
     },
     {
       title: "Resume Screener Website",
       description: "Website to evaluate resume against key criteria for hiring managers",
-      imgUrl: projImg2,
+      imgUrl: resume_screener,
     },
     {
       title: "Inventory Management System",
       description: "Website for workers to fully take control over there inventory system",
-      imgUrl: projImg2,
+      imgUrl: stock_manage,
     },
   ];
 
@@ -44,50 +66,34 @@ export const Projects = () => {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Here are some of the projects I've worked on, showcasing my skills and creativity. Explore the work I've done to solve real-world challenges and bring ideas to life. Feel free to take a look and let me know what you think!</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
-                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
-                    <Nav.Item>
-                      <Nav.Link eventKey="first">Projects</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="second">About Me</Nav.Link>
-                    </Nav.Item>
-                    <Nav.Item>
-                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
-                    </Nav.Item>
-                  </Nav>
-                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
-                    <Tab.Pane eventKey="first">
-                      <Row>
-                        {
-                          projects.map((project, index) => {
-                            return (
-                              <ProjectCard
-                                key={index}
-                                {...project}
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projects</h2>
+                  <p>Here are some of the projects I've worked on, showcasing my skills and creativity. Explore the work I've done to solve real-world challenges and bring ideas to life. Feel free to take a look and let me know what you think!</p>
+                  <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                    <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                      <Tab.Pane eventKey="first">
+                        <Row>
+                          {
+                            projects.map((project, index) => {
+                              return (
+                                <ProjectCard
+                                  key={index}
+                                  {...project}
                                 />
-                            )
-                          })
-                        }
-                      </Row>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="section">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                    <Tab.Pane eventKey="third">
-                      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
-                    </Tab.Pane>
-                  </Tab.Content>
-                </Tab.Container>
-              </div>}
+                              )
+                            })
+                          }
+                        </Row>
+                      </Tab.Pane>
+                    </Tab.Content>
+                  </Tab.Container>
+                </div>
+              }
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
       <img className="background-image-right" src={colorSharp2}></img>
     </section>
-  )
-}
+  );
+}  
